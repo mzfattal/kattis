deb88a63e7b8c43bb3d75ac01eade1fa58f6c5bd
Mutaz Fattal <mzfattal@hotmail.com>
2020-07-30 13:44:00 -0400

    TakeTwoStones

---
+++ src/TakeTwoStones.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TakeTwoStones {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        if(n%2==0) {
+            System.out.println("Bob");
+        } else {
+            System.out.println("Alice");
+        }
+
+    }
+}
