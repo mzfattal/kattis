2d6c0f61034e87156261cd10510ac92418a5c00c
Mutaz Fattal <mzfattal@hotmail.com>
2020-07-30 13:09:18 -0400

    SolvingForCarrots

---
+++ src/SolvingForCarrots.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class SolvingForCarrots {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.nextInt();
+        int carrots = in.nextInt();
+        System.out.print(carrots);
+    }
+}
